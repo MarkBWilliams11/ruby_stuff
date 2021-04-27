175caf9389d95ed52c174380838702063da01d76
MarkBWilliams <mark.b.will1@gmail.com>
2021-04-27 15:08:09 -0700

    exercises with enumerables and a quick read on transducers

--- transducers.rb
+++ transducers.rb
@@ -16,7 +16,8 @@ p [0, 1, 2, 3, 4, 5]
   .map    { |x| x + 1   }
   .select { |x| x.even? }
   # => [2, 4,6]
-
+  
+#select_map like method form with reduce
   def select_map(list,&function)
     list.reduce([]){ |acc,val|
       result= function.call(val)
@@ -25,4 +26,4 @@ p [0, 1, 2, 3, 4, 5]
     }
   end
   p select_map([1,2,3]) { |v| v} #=> [1,2,3]
-#map with reduce 
+
